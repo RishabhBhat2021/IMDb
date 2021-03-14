4be625caffa007090c82a42dbaf0fdfe8fa738e8
Rishabh Bhat <rishabhbhat20@gmail.com>
2021-03-14 11:14:22 +0530

    Made changes in the UI and added a loading screen in the imdb(OOP) file

--- imdb (OOP).py
+++ imdb (OOP).py
@@ -3,6 +3,7 @@ import threading
 
 import tkinter as tk
 from tkinter import ttk
+from tkinter.constants import CENTER
 
 from PIL import ImageTk, Image
 
@@ -31,7 +32,7 @@ class ImdbApp(tk.Tk):
         # Use self.image for images 
         self.imdb_logo = ImageTk.PhotoImage(Image.open("imdb logo.png"))
         imdb_logo_label = tk.Label(self, image=self.imdb_logo, bg="black")
-        imdb_logo_label.pack(padx=10,pady=10)        
+        imdb_logo_label.pack(padx=10,pady=(20,10))        
 
         container = tk.Frame(self)
         container.pack(side="top", fill="both", expand=True)
@@ -40,7 +41,7 @@ class ImdbApp(tk.Tk):
 
         self.frames = {}
 
-        for F in (StartPage, HistoryPage):
+        for F in (StartPage, SearchPage, HistoryPage, LoadingPage):
             frame = F(container, self)
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky="nsew")
@@ -65,25 +66,62 @@ class StartPage(tk.Frame):
         tk.Frame.configure(self, bg="black")
 
         self.controller = controller
-
-        self.my_entry = tk.Entry(self, font=MEDIUM_FONT)
-        self.my_entry.pack(pady=10)
         
-        my_button = ttk.Button(self, text="Search", command=lambda: threading.Thread(target=self.search_title).start())
-        my_button.pack(pady=10)
+        buttons_frame = tk.Frame(self, bg="black")
+        buttons_frame.pack(pady=10)
 
-        history_button = ttk.Button(self, text="Show History", command=lambda: controller.show_frame(HistoryPage))
-        history_button.pack(pady=10)
+        self.my_entry = tk.Entry(buttons_frame, font=MEDIUM_FONT)
+        self.my_entry.pack(padx=10, pady=10, side=tk.LEFT)
 
-        self.display_frame = tk.Frame(self, bg="black")
+        my_button = ttk.Button(buttons_frame, text="Search", command = self.search)
+        my_button.pack(padx=10, pady=10, side=tk.LEFT)
 
-        self.title_label = tk.Label(self.display_frame, font=BOLD_LARGE_FONT, bg="black", fg="white")
+        history_button = ttk.Button(buttons_frame, text="Show History", command=lambda: controller.show_frame(HistoryPage))
+        history_button.pack(padx=10, pady=10, side=tk.RIGHT)
 
+    def search(self):
+
+        self.controller.show_frame(LoadingPage)
+
+        search_page = self.controller.get_page(SearchPage)
+
+        threading.Thread(target=search_page.search_title).start()
+
+
+class SearchPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+
+        tk.Frame.__init__(self, parent)
+        tk.Frame.configure(self, bg="black")
+
+        self.controller = controller
+
+        home_button = ttk.Button(self, text="Go Back", command=lambda: controller.show_frame(StartPage))
+        home_button.pack(pady=10)
+
+        self.main_frame = tk.Frame(self, bg="black")
+
+        self.title_frame = tk.Frame(self.main_frame, bg="black")
+
+        self.title_label = tk.Label(self.title_frame, font=BOLD_LARGE_FONT, bg="black", fg="white")
+        self.poster_label = tk.Label(self.title_frame, bd=0, bg="black")
+
+        self.cast_frame = tk.Frame(self.main_frame, bg="black")
+
+        self.cast_textbox = tk.Text(self.cast_frame, bg="black", fg="white", font=MEDIUM_FONT, width=30, height=15)
+        self.cast_textbox.tag_configure("center", justify='center')   # To center allign the text
+        self.cast_textbox.tag_add("center", "1.0", "end")             # in the Text Box: cast_textbox
+
+        # make a scrollbar
+        self.scrollbar = tk.Scrollbar(self.main_frame)
+
+        self.cast_heading_label = tk.Label(self.cast_frame, text='Cast', bg='black', fg='white', font=BOLD_MEDIUM_FONT)
+        
     def search_title(self):
 
-        self.display_frame.pack_forget()
-        
-        self.title = self.my_entry.get()
+        home_page = self.controller.get_page(StartPage)
+        self.title = home_page.my_entry.get()
 
         result = requests.get(f'https://www.imdb.com/find?q={self.title}&s=tt&ref_=fn_al_tt_mr')
         src = result.content
@@ -94,9 +132,9 @@ class StartPage(tk.Frame):
         self.title_name = tag.text
 
         title_id = tag.a.attrs['href']
-        title_link = f'https://www.imdb.com/{title_id}'
+        self.title_link = f'https://www.imdb.com/{title_id}'
 
-        result_title = requests.get(title_link)
+        result_title = requests.get(self.title_link)
         src_title = result_title.content
         soup_title = BeautifulSoup(src_title, 'lxml')
         tag_title = soup_title.find('div', class_='ratingValue')
@@ -106,52 +144,49 @@ class StartPage(tk.Frame):
 
         # Title Name
         self.title_label.config(text=f'{self.title_name}\nIMDb: {self.title_rating}')
-        self.title_label.grid(row=0, column=1)
+        self.title_label.pack(pady=10)
 
         # Poster
-        tag_img = soup_title.find('img')
-        poster_link = tag_img.attrs['src']
+        self.tag_img = soup_title.find('img')
+        self.poster_link = self.tag_img.attrs['src']
         # Retreiving the Image using urllib
-        urllib.request.urlretrieve(poster_link, 'poster.png')  
+        urllib.request.urlretrieve(self.poster_link, 'poster.png')  
 
         self.poster = ImageTk.PhotoImage(Image.open("poster.png"))
-        poster_label = tk.Label(self.display_frame, image=self.poster, bd=0, bg="black")
-        poster_label.grid(row=0,column=0)
+        self.poster_label.config(image=self.poster)
+        self.poster_label.pack(pady=10)
 
         # Cast
-        self.cast_list = []
+        self.cast_list = ""
 
-        self.cast_frame = tk.Frame(self.display_frame, bg="black")
-        self.cast_frame.grid(row=2, column=0, columnspan=2)
+        self.cast_heading_label.pack(padx=20, pady=(20,15))
 
-        cast_heading_label = tk.Label(self.cast_frame, text='Cast', bg='black', fg='white', font=BOLD_MEDIUM_FONT)
-        cast_heading_label.pack(padx=20, pady=(20,0))
-
-        tags_cast = soup_title.find_all('td',class_='primary_photo')
-
-        self.cast_textbox = tk.Text(self.cast_frame, bg="black", fg="white")
-        self.cast_textbox.pack()
-
-        # make a scrollbar
-        scrollbar = tk.Scrollbar(self.cast_frame)
-        scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        self.tags_cast = soup_title.find_all('td',class_='primary_photo')
+        
+        self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
         # Configuring the ScrollBar
-        scrollbar.config(command=self.cast_textbox.yview)
+        self.scrollbar.config(command=self.cast_textbox.yview)
 
-        self.cast_textbox.config(yscrollcommand=scrollbar.set)
+        self.cast_textbox.config(yscrollcommand=self.scrollbar.set)
 
-        for tag_cast in tags_cast:
-            cast_list = tag_cast.a.img.attrs['alt'] + "\n"
-            self.cast_textbox.insert(tk.END, cast_list)
+        self.cast_textbox.delete("1.0", tk.END)
+
+        for self.tag_cast in self.tags_cast:
+            self.cast_list = self.tag_cast.a.img.attrs['alt'] + "\n"
+            self.cast_textbox.insert("1.0", self.cast_list, "center")
+
+        self.cast_frame.pack(padx=10, side=tk.RIGHT)
+        self.cast_textbox.pack()
+        self.title_frame.pack(pady=10, padx=10, side=tk.LEFT)
+        self.main_frame.pack(pady=10)
 
         history_page = self.controller.get_page(HistoryPage)
         history = self.title_name + " " + self.title_rating + "\n"
         history_page.history_log.insert(tk.END, history)
 
-        self.display_frame.pack()
+        self.controller.show_frame(SearchPage)
 
-        os.remove("poster.png")
 
 class HistoryPage(tk.Frame):
 
@@ -160,20 +195,41 @@ class HistoryPage(tk.Frame):
         tk.Frame.__init__(self, parent)
         tk.Frame.configure(self, bg="black")
 
-        self.history_log = tk.Text(self, bg="black", fg="white")
+        self.controller = controller
+
+        history_label = tk.Label(self, text="Search History", bg="black", fg="white", font=MEDIUM_FONT)
+        history_label.pack(pady=10, side=tk.TOP)
+
+        self.history_log = tk.Text(self, bg="black", fg="white", font=40)
         self.history_log.pack(fill="both", expand=True)
 
         home_button = ttk.Button(self, text="Return to Home", command=lambda: controller.show_frame(StartPage))
-        home_button.pack(pady=10, side="bottom")
+        home_button.pack(pady=10, side=tk.BOTTOM)
 
-        history_label = tk.Label(self, text="Search History", bg="black", fg="white", font=MEDIUM_FONT)
-        history_label.pack(pady=10)
+    def delete_last(self):
+
+        search_page = self.controller
+        search_page.pack_forget()
+
+
+class LoadingPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+
+        tk.Frame.__init__(self, parent)
+        tk.Frame.configure(self, bg="black")
+
+        self.controller = controller
+
+        loading_label = tk.Label(self, text="Searching...\nPlease Wait", bg="black", fg="white", font=BOLD_MEDIUM_FONT)
+        loading_label.pack()
 
 
 def main():
     
     app = ImdbApp()
     app.mainloop()
+    os.remove("poster.png")
 
 if __name__ == "__main__":
     main()
